0fe22e7691874065fbed7ba4af7c84bbabdbc93b
Pinal Bhatt <Pinal.Bhatt@weightwatchers.com>
2015-05-16 18:18:26 -0400

    gulp utils

--- index.js
+++ index.js
@@ -1,14 +1,63 @@
 (function(){
     'use strict';
 
-    module.exports = function(gulp, src, dest){
+    module.exports = function(gulp){
 
-        var utils = require('./utils/utils')(gulp);
-        var tasks = require('./utils/tasks')(gulp, src, dest);
-        return {
-            utils: utils,
-            tasks: tasks
+        var args = require('yargs').argv;
+        var del = require('del');
+        var GP = require('gulp-load-plugins')({lazy:true});
+
+        var gUtil = {
+            G: gulp,
+            GP: GP,
+            args: args,
+            log: _log,
+            logV: _logV,
+            copyFiles: _copyFiles,
+            clean: _clean
         };
+        return gUtil;
+
+        function _log(msg){
+            if(typeof(msg) === 'object'){
+                for(var item in msg){
+                    if(msg.hasOwnProperty(item)) {
+                        GP.util.log(GP.util.colors.blue(msg[item]));
+                    }
+                }
+            }
+            else {
+                GP.util.log(GP.util.colors.yellow(msg));
+            }
+        }
+
+        function _logV(msg){
+            if(args.verbose) {
+                _log(msg);
+            }
+        }
+
+        function _copyFiles(source, dest){
+            _logV('Copying ' + source  + ' to ' + dest);
+            return gulp
+                .src(source)
+                .pipe(gulp.dest(dest));
+        }
+
+        function _clean(src){
+            _logV('Deleting: ' + src);
+            del(src, function(err, deletedFiles){
+                if(err){
+                    _log('Error cleaning : in _clean call');
+                    _log(err);
+                }
+                else {
+                    _logV(deletedFiles.length +  ' Files deleted:');
+                    _logV(deletedFiles.join(', '));
+                }
+            });
+        }
+
     };
 
 }());
